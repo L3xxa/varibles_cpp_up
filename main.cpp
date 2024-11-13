11b6551223c08de64125d2407f3225aaa2fb7e61
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-11-13 22:50:21 +0200

    Complete ex_2

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
 int main() {
-    int year; // Declare the variable year
-    cout << "Enter a year: ";
-    cin >> year;
-    int days = 365 + (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
-    cout << "the number of days in the year " << year << " is " << days << endl;
+	int sec, hour, time;
+	cout << "Enter the number of seconds: ";
+	cin >> sec;
+	hour = sec / 3600;
+	time = 8 - hour;
+	cout << "Home in: " << time << "hours" << endl;
 
     return 0;
 }
