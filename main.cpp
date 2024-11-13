b0e375f35fa65caf15d428889873aea690bce5d9
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-11-13 22:04:27 +0200

    complete ex_3

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
 int main() {
-    int year; // Declare the variable year
-    cout << "Enter a year: ";
-    cin >> year;
-    int days = 365 + (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
-    cout << "the number of days in the year " << year << " is " << days << endl;
+	cout << "\033[1;31mCalculation of the volume of the parallelepiped.\033[0m" << endl;
+	cout << "\033[1;31mEnter the output data:\033[0m" << endl;
+	double L, W, H, V;
+	cout << "\033[1;33mLength (cm) => \033[0m";
+	cin >> L;
+	cout << "\033[1;33mWidth (cm) => \033[0m";
+	cin >> W;
+	cout << "\033[1;33mHeight (cm) => \033[0m";
+	cin >> H;
+	V = L * W * H;
+	cout << "\033[1;33mVolume (cm^3) => \033[0m" << V << endl;
 
-    return 0;
+	return 0;
 }
